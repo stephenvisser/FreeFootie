261daf29049b3865f9ec5cd6c127312774c1c7c7
Stephen Visser <visser.stephen@gmail.com>
2013-09-30 21:57:30 -0600

    Changed according to new specs from Tim

--- app/scripts/controllers/standings.js
+++ app/scripts/controllers/standings.js
@@ -3,25 +3,47 @@
 angular.module('freefootieApp')
   .controller('StandingsCtrl', function ($scope) {
 
-$scope.standings = [
-  		{
-  			name: 'Ronald\'s Rhutabagas',
-  			wins: 5,
-  			losses: 3,
-  			ties: 0
-  		},
-      {
-        name: 'Matilda\'s Moose',
-        wins: 3,
-        losses: 3,
-        ties: 2
-      },
-      {
-        name: 'Dave\'s Ducks',
-        wins: 0,
-        losses: 5,
-        ties: 3
-      },
-  	]; 
+$scope.standings = {
+  'North Pool': [
+    {
+			name: 'Ronald\'s Rhutabagas',
+			wins: 5,
+			losses: 3,
+			ties: 0
+		},
+    {
+      name: 'Matilda\'s Moose',
+      wins: 3,
+      losses: 3,
+      ties: 2
+    },
+    {
+      name: 'Dave\'s Ducks',
+      wins: 0,
+      losses: 5,
+      ties: 3
+    }
+  ],
+  'South Pool': [
+    {
+      name: 'Ronald\'s Rhutabagas',
+      wins: 5,
+      losses: 3,
+      ties: 0
+    },
+    {
+      name: 'Matilda\'s Moose',
+      wins: 3,
+      losses: 3,
+      ties: 2
+    },
+    {
+      name: 'Dave\'s Ducks',
+      wins: 0,
+      losses: 5,
+      ties: 3
+    }
+  ]
+  }; 
 
   });
