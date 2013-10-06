19148dff98aceb8355e19a0fc5c8ef3483c5d278
Curtis Badke <curtis@badkid.ca>
2013-10-06 15:25:01 -0600

    Convert refs controller to use new data API calls

--- app/scripts/controllers/ref.js
+++ app/scripts/controllers/ref.js
@@ -1,22 +1,39 @@
 'use strict';
 
 angular.module('freefootieApp')
-  .controller('RefCtrl', function ($scope) {
+  .controller('RefCtrl', function ($scope, $resource) {
 
-    $scope.currentGame = null;
+      $scope.currentGame = null;
 
-  	$scope.games = [
-  		{
-  			location: 'St Mary\'s',
-  			time: new Date(),
-  			home: 'Timmy\'s Tigers',
-  			away: 'Bobby\'s Brontasauruses'
-  		},
-      {
-        location: 'Downtown Core',
-        time: new Date(),
-        home: 'Susan\'s Ostriches',
-        away: 'Ronald\'s Rhutabagas'
-      }
-  	]; 
+      //NOTE: the API and sample data have not concept of people and therefore
+      //not concept of referees or players. These will need to be added in order
+      //to flesh this controller out properly.
+      //
+      var teamsSrc = $resource('/api/teams/');
+      var locationsSrc = $resource('/api/locations/');
+      var gamesSrc = $resource('/api/games/');
+
+      gamesSrc.query(function(games) {
+          locationsSrc.query(function(locations){
+              var locationNames = {};
+              locations.forEach(function (l){
+                  locationNames[l.id] = l.name;
+              });
+
+              teamsSrc.query(function(teams){
+                  var teamNames = {};
+                  teams.forEach(function (t){
+                      teamNames[t.id] = t.name;
+                  });
+
+                  games.forEach(function (g) {
+                      g.location = locationNames[g.location];
+                      g.time = new Date(g.time);
+                      g.home = teamNames[g.home];
+                      g.away = teamNames[g.away];
+                  });
+                  $scope.games = games.slice(0,2);
+              });
+          });
+      });
   });
