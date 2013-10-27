9677cd839bea2f6816aa4efef2a8296740d9e078
Stephen Visser <visser.stephen@gmail.com>
2013-10-27 16:47:31 -0600

    Added ability to edit the team

--- app/scripts/controllers/team-details.js
+++ app/scripts/controllers/team-details.js
@@ -7,6 +7,8 @@ angular.module('freefootieApp')
     var Pool = $resource('/api/pools/:id');
     var Player = $resource('/api/players/:id');
 
+    $scope.edit = {editing: false};
+
     $scope.currentTeam = Team.get({id: $routeParams.id}, function(team) {
     	$scope.pool = Pool.get({id: team.pool});
     	$scope.players = team.players.map(function(player){
