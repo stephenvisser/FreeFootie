13559ac029b81d0b0d1043f7b253caee11833a07
Curtis Badke <curtis@badkid.ca>
2013-11-15 08:22:04 -0700

    We don't use Apache. Get rid of the config for it.

--- Gruntfile.js
+++ Gruntfile.js
@@ -299,7 +299,6 @@ module.exports = function (grunt) {
           dest: '<%= yeoman.dist %>',
           src: [
             '*.{ico,png,txt}',
-            '.htaccess',
             'bower_components/**/*',
             'images/{,*/}*.{gif,webp}',
             'styles/fonts/*'
