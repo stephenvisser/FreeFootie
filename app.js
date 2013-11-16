f8ac2c8ecbc3233f86f8ae527a95e287b2feba1f
Curtis Badke <curtis@badkid.ca>
2013-11-16 16:27:42 -0700

    Harden client/server split.
    Bring in express to handle the serving. Move REST API stuff into server
    code instead of the Grunt. Heavily stripped out the yeoman generated
    tasks in the Gruntfile. They can be reintroduced as needed and people
    have a stronger understanding of what they do.

---
+++ app.js
@@ -0,0 +1,48 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , http = require('http')
+  , path = require('path')
+  , settings = require('./settings')
+  , routeConfig = require('./server/route-config');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || settings.webserver.port || 9000);
+app.set('views', __dirname + '/server/views');
+app.use(express.favicon(__dirname + '/client/favicon.ico'));
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+
+// load liveReload script only in development mode
+// load before app.router
+app.configure('development', function() {
+  // live reload script
+  var liveReloadPort = settings.liveReload.port || 35729;
+  var excludeList = ['.woff', '.flv'];
+
+  app.use(require('connect-livereload')({
+    port: liveReloadPort,
+    excludeList: excludeList
+  }));
+});
+
+app.use(app.router);
+app.use(require('less-middleware')({ src: __dirname + '/client' }));
+app.use(express.static(path.join(__dirname, 'client')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+}
+
+routeConfig.configureRoutes(app);
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+  console.log('express server listening on port ' + app.get('port'));
+});
