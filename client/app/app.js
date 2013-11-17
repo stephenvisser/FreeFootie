62b81bd8d95814bb5568ef8d0316aa1945f59cb4
Curtis Badke <curtis@badkid.ca>
2013-11-17 10:16:12 -0700

    Fix whitespace in app.js

--- client/app/app.js
+++ client/app/app.js
@@ -4,47 +4,47 @@ angular.module('freefootieApp', ['ngResource', 'google-maps', 'ngRoute'])
   .config(function ($routeProvider) {
     $routeProvider
     .when('/ref/game/:id', {
-        templateUrl: 'app/views/ref-details.html',
-        controller: 'RefDetailsCtrl'
-      })
-      .when('/ref', {
-        templateUrl: 'app/views/ref.html',
-        controller: 'RefCtrl'
-      })
-       .when('/team', {
-        templateUrl: 'app/views/team.html',
-        controller: 'TeamCtrl'
-      })
-        .when('/admin', {
-            templateUrl: 'app/views/admin.html',
-            controller: 'AdminCtrl'
-        })
-        .when('/admindetails/:id', {
-            templateUrl: 'app/views/admin-game-details.html',
-            controller: 'AdminDetailsCtl'
-        })
-        .when('/standings', {
-            templateUrl: 'app/views/standings.html',
-            controller: 'StandingsCtrl'
-        })
-        .when('/admin/team/:id', {
-            templateUrl: 'app/views/team-details.html',
-            controller: 'TeamDetailsCtrl'
-        })
-        .when('/admin/team', {
-            templateUrl: 'app/views/team-admin.html',
-            controller: 'TeamAdminCtrl'
-        })
-        .when('/scheduler', {
-            templateUrl: 'app/views/scheduler.html',
-            controller: 'SchedulerCtrl'
-        })
-        .otherwise({
-            redirectTo: '/admin'
-        });
+      templateUrl: 'app/views/ref-details.html',
+      controller: 'RefDetailsCtrl'
+    })
+    .when('/ref', {
+      templateUrl: 'app/views/ref.html',
+      controller: 'RefCtrl'
+    })
+    .when('/team', {
+      templateUrl: 'app/views/team.html',
+      controller: 'TeamCtrl'
+    })
+    .when('/admin', {
+      templateUrl: 'app/views/admin.html',
+      controller: 'AdminCtrl'
+    })
+    .when('/admindetails/:id', {
+      templateUrl: 'app/views/admin-game-details.html',
+      controller: 'AdminDetailsCtl'
+    })
+    .when('/standings', {
+      templateUrl: 'app/views/standings.html',
+      controller: 'StandingsCtrl'
+    })
+    .when('/admin/team/:id', {
+      templateUrl: 'app/views/team-details.html',
+      controller: 'TeamDetailsCtrl'
+    })
+    .when('/admin/team', {
+      templateUrl: 'app/views/team-admin.html',
+      controller: 'TeamAdminCtrl'
+    })
+    .when('/scheduler', {
+      templateUrl: 'app/views/scheduler.html',
+      controller: 'SchedulerCtrl'
+    })
+    .otherwise({
+      redirectTo: '/admin'
+    });
   }).run(function($rootScope, $location){
     $rootScope.go = function(path) {
-        $location.path(path);
-      };
+      $location.path(path);
+    };
   });
 
