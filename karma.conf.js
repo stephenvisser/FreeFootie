a9a3df7597aa0b582da853e163313260343e286d
Curtis Badke <curtis@badkid.ca>
2013-11-17 11:50:03 -0700

    Strip remaining signs of angular-google library

--- karma.conf.js
+++ karma.conf.js
@@ -12,7 +12,6 @@ module.exports = function(config){
         'client/bower_components/angular-resource/angular-resource.js',
         'client/bower_components/angular-route/angular-route.js',
         'client/bower_components/angular-mocks/angular-mocks.js',
-        'client/bower_components/angular-google-maps/src/angular-google-maps.js',
         'client/app/app.js',
         'client/app/**/*.js',
         'test/**/*.js'
