112fe505c7e5725ba4cd57ff04632a7c53055ba8
Curtis Badke <curtis@badkid.ca>
2013-11-17 10:50:41 -0700

    Reorg angular files into areas

--- client/app/app.js
+++ client/app/app.js
@@ -4,39 +4,39 @@ angular.module('freefootieApp', ['ngResource', 'google-maps', 'ngRoute'])
   .config(function ($routeProvider) {
     $routeProvider
     .when('/ref/game/:id', {
-      templateUrl: 'app/views/ref-details.html',
+      templateUrl: 'app/ref/views/ref-details.html',
       controller: 'RefDetailsCtrl'
     })
     .when('/ref', {
-      templateUrl: 'app/views/ref.html',
+      templateUrl: 'app/ref/views/ref.html',
       controller: 'RefCtrl'
     })
     .when('/team', {
-      templateUrl: 'app/views/team.html',
+      templateUrl: 'app/ref/views/team.html',
       controller: 'TeamCtrl'
     })
     .when('/admin', {
-      templateUrl: 'app/views/admin.html',
+      templateUrl: 'app/admin/views/admin.html',
       controller: 'AdminCtrl'
     })
     .when('/admindetails/:id', {
-      templateUrl: 'app/views/admin-game-details.html',
+      templateUrl: 'app/admin/views/admin-game-details.html',
       controller: 'AdminDetailsCtl'
     })
     .when('/standings', {
-      templateUrl: 'app/views/standings.html',
+      templateUrl: 'app/public/views/standings.html',
       controller: 'StandingsCtrl'
     })
     .when('/admin/team/:id', {
-      templateUrl: 'app/views/team-details.html',
+      templateUrl: 'app/admin/views/team-details.html',
       controller: 'TeamDetailsCtrl'
     })
     .when('/admin/team', {
-      templateUrl: 'app/views/team-admin.html',
+      templateUrl: 'app/admin/views/team-admin.html',
       controller: 'TeamAdminCtrl'
     })
-    .when('/scheduler', {
-      templateUrl: 'app/views/scheduler.html',
+    .when('/admin/scheduler', {
+      templateUrl: 'app/admin/views/scheduler.html',
       controller: 'SchedulerCtrl'
     })
     .otherwise({
