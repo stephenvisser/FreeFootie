1575b5d85ea89a82fc3f3197990af47f5a199771
Stephen Visser <visser.stephen@gmail.com>
2014-04-12 15:54:34 -0600

    There were some missing changes from the gulpfile update

--- server/staticServerService.js
+++ server/staticServerService.js
@@ -6,31 +6,14 @@ function abToStr(arrayBuffer) {
 }
 
 module.exports = function initStaticServer(env) {
-  var deferred = q.defer();
+  return function() {
+    var server = nodemon({
+      script: 'server.js',
+      //We don't want nodemon to watch all js files; whitelist 'server'
+      ignore: '*',
+      env: env
+    });
 
-  var reject = function(err){
-    deferred.reject(err);
+    return q.when(server.restart.bind(server));
   }
-
-  var server = nodemon({
-    script: 'server.js',
-    //We don't want nodemon to watch all js files; whitelist 'server'
-    ignore: '*',
-    env: env
-  });
-
-  server.on('stdout', function(out){
-    var result = abToStr(out);
-    if (result.match('server listening on port')) {
-      //Since the 'start' nodemon event isn't emitted when the server STARTS,
-      //we use a very fragile listener that depends on stdout format
-      deferred.resolve(server);
-    }
-  });
-
-  server.on('crash', reject);
-
-  server.on('exit', reject);
-
-  return deferred.promise;
 };
